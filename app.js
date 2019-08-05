6d5a7d93974535d555aff766b6d2020d761e154a
Birdevinder Singh <Veer davinder>
2019-08-05 16:29:17 -0400

    JS Hover Effect

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent:document.querySelector(".distortion"),
+    intensity:0.2,
+    image1:"./images/image_2.jpg",
+    image2:"./images/image_1.jpg",
+    displacementImage:"./images/heightMap.png"
+});
